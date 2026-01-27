60f620f050edba9f06728d1165ff4e18efc7cdc2
H-ALIUEIA <jipnimaialiuia@gmail.com>
2026-01-27 04:29:12 +0200

    Update views.py

--- api/views.py
+++ api/views.py
@@ -84,7 +84,7 @@ def get_short(request):
  
 @api_view(['GET'])
 def get_version(request):
- return Response({'version': '1'})
+ return Response({'version': '2'})
  
 @api_view(['GET'])
 def get_level_num(request, pk):
